5eff98cc407f8d40bd400d9d478f82b90703700c
Paul <paul.nicolezim@gmail.com>
2026-09-23 19:38:26 -0700

    Export starting population preview helper

--- crates/evolab-core/src/lib.rs
+++ crates/evolab-core/src/lib.rs
@@ -53,6 +53,7 @@ pub use fitness::{FitnessConfig, FitnessMetrics, FitnessResult, FitnessWeights, 
 pub use ga::{
     EvaluatedCreature, EvolutionConfig, EvolutionResult, GenerationSummary, GenerationTiming,
     OffspringTelemetry, PopulationTelemetry, evolve_population, evolve_population_checkpointed,
+    generate_initial_population_preview,
 };
 pub use rapier_cpu::RapierCpuBackend;
 pub use results::{
