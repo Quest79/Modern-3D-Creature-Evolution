803ec16f52a812e3f7ad1e3ece5a3d6d699e0ed5
Paul <paul.nicolezim@gmail.com>
2026-09-25 19:42:27 -0700

    Restore known-good simulation while keeping RAM transport

--- crates/evolab-core/src/lib.rs
+++ crates/evolab-core/src/lib.rs
@@ -40,13 +40,9 @@ pub use creature::{
     BIOLOGICAL_MAX_CONTACT_FRICTION, BIOLOGICAL_MAX_CYCLIC_POWER_W_PER_KG,
     BIOLOGICAL_MAX_DENSITY_KG_M3, BIOLOGICAL_MAX_MUSCLE_STRESS_PA, BIOLOGICAL_MIN_DENSITY_KG_M3,
     BiologicalMaterial, CreatureBodySnapshot, CreatureGenome, CreatureReport, CreatureSimulator,
-    CreatureSnapshot, CreatureVisualSession, JointGene, MAX_VALID_LINEAR_SPEED_M_S,
-    MUSCLE_DENSITY_KG_M3, SegmentGene,
+    CreatureSnapshot, JointGene, MAX_VALID_LINEAR_SPEED_M_S, MUSCLE_DENSITY_KG_M3, SegmentGene,
 };
-pub use cuda_creature::{
-    CudaCreatureBatchResult, CudaCreatureVisualBatchResult, CudaCreatureVisualFrame,
-    run_cuda_creature_batch, run_cuda_creature_visual_batch,
-};
+pub use cuda_creature::{CudaCreatureBatchResult, run_cuda_creature_batch};
 pub use cuda_probe::{CudaProbeBatchReport, discover_cuda_devices, run_cuda_probe_batch};
 pub use evolution::{
     GenomeRng, MutationConfig, MutationKind, MutationRecord, MutationResult,
